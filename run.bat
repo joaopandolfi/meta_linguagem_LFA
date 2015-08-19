2dc71c146fd87169ccaadb08168dbd88d65c455b
Joao Carlos <joaopandolfi@gmail.com>
2015-08-19 00:31:35 -0300

    Run pelo terminal Linux, Mac -> [./run.sh arquivo] e Windows -> [run.bat arquivo].

--- run.bat
+++ run.bat
@@ -3,3 +3,4 @@ echo Copiando executavel
 copy libs\src\out\artifacts\TrabLFA_jar\TrabLFA.jar bin\run.jar
 echo executando
 java -jar bin\run.jar < %1
+echo ===\> SIDA: ino/ino.ino
---
+++ run.sh
@@ -0,0 +1,5 @@
+echo Copiando executavel
+cp libs/src/out/artifacts/TrabLFA_jar/TrabLFA.jar bin/run.jar
+echo executando
+java -jar bin/run.jar < $1
+echo ===\> SIDA: ino/ino.ino
