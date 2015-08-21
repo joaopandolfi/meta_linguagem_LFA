7d60e73bd4e6245e4d6eb69a6c1771aa7bc26cbf
Joao Carlos <joaopandolfi@gmail.com>
2015-08-21 01:46:46 -0300

    Interface gráfica para windows

--- run.bat
+++ run.bat
@@ -3,4 +3,5 @@ echo Copiando executavel
 copy libs\src\out\artifacts\TrabLFA_jar\TrabLFA.jar bin\run.jar
 echo executando
 java -jar bin\run.jar < %1
-echo ===\> SAIDA: ino/ino.ino
+echo === SAIDA: ino/ino.ino
+pause
